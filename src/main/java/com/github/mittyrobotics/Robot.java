634535cde77d4f2516e12d6f9913b6b5b13bb7ef
Akshay-Praveen <akshaypraveen26@mittymonarch.com>
2022-11-04 16:14:30 -0700

    test

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -1,12 +1,20 @@
 package com.github.mittyrobotics;
 
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 
 public class Robot extends TimedRobot {
+    XboxController controller;
+    WPI_TalonSRX motor;
+
     @Override
     public void robotInit() {
+        motor = new WPI_TalonSRX(24);
+        motor.configFactoryDefault();
+        controller = new XboxController(0);
 
     }
 
@@ -36,6 +44,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+        motor.set(controller.getLeftY());
     }
 
     @Override
