6e248e3239c00dc5792e11f44b6367c53cd1f7c6
varinik <87580597+varinik@users.noreply.github.com>
2022-11-04 15:55:17 -0700

    Update Robot.java

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -1,13 +1,20 @@
 package com.github.mittyrobotics;
 
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 
 public class Robot extends TimedRobot {
+    WPI_TalonSRX motor;
+    XboxController controller;
+
     @Override
     public void robotInit() {
-
+        motor = new WPI_TalonSRX(24);
+        motor.configFactoryDefault();
+        controller = new XboxController(0);
     }
 
     @Override
@@ -36,6 +43,8 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+        motor.set(controller.getLeftY());
+
     }
 
     @Override
