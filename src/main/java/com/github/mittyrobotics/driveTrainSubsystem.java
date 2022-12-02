e9d269e545cc2c7869f6c9610b48cd8ebfc06acb
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-12-02 15:18:25 -0800

    dirvetrainstartiwhtinithardware

---
+++ src/main/java/com/github/mittyrobotics/driveTrainSubsystem.java
@@ -0,0 +1,30 @@
+package com.github.mittyrobotics;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class driveTrainSubsystem extends SubsystemBase {
+    static driveTrainSubsystem ourInstance = new driveTrainSubsystem();
+    public static driveTrainSubsystem getInstance() {return ourInstance};
+
+    private driveTrainSubsystem() {
+        //with properties and the such
+        super();
+        setName("Drive Train Subsystem");
+    }
+
+    WPI_TalonFX tlMotor, blMotor, trMotor, brMotor;
+
+   WPI_TalonFX[] motors = {tlMotor, blMotor, trMotor, brMotor};
+
+    public void initHardware() {
+        tlMotor = new WPI_TalonFX(5);
+        trMotor = new WPI_TalonFX(6);
+        blMotor = new WPI_TalonFX(7);
+        brMotor = new WPI_TalonFX(8);
+        for (WPI_TalonFX mot : motors) {
+            mot.configFactoryDefault();
+        }
+
+    }
+}
