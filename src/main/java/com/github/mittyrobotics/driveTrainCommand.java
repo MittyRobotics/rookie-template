e1ea5e66d93be2f66a74a87d59c1ce953f547c35
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-12-02 16:03:31 -0800

    finished12

--- src/main/java/com/github/mittyrobotics/driveTrainCommand.java
+++ src/main/java/com/github/mittyrobotics/driveTrainCommand.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 //note to self; really interesitng since:
 // no controller input;
 // turns infinitely;
-// if adds controller to subsystem will reinstnatiate for each usbsystme; have to acces s once controler from a main or a different class to read
+// if adds controller to subsystem will reinstnatiate; have to acces s once controler from a different class to read
 // cant really make drive func, ca njust put everything in periodic if adding float values to a variable
 
 public class driveTrainCommand extends CommandBase {
