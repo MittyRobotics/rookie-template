2b3a3b9bc5c8c218393cabcef266794895844dd7
VKRobotics <113646962+VKRobotics@users.noreply.github.com>
2022-12-08 16:56:03 -0800

    arcade drive - updated

--- src/main/java/com/github/mittyrobotics/Robot2.java
+++ src/main/java/com/github/mittyrobotics/Robot2.java
@@ -11,13 +11,23 @@ public class Robot2 extends TimedRobot {
     XboxController controller;
     WPI_TalonSRX motor;
     WPI_TalonSRX motor2;
+    WPI_TalonSRX motor3;
+    WPI_TalonSRX motor4;
+
     public void robotInit() {
         motor= new WPI_TalonSRX(24);
         motor.configFactoryDefault();
         controller=new XboxController(8);
         motor2= new WPI_TalonSRX(26);
         motor2.configFactoryDefault();
-        motor.setInverted(true);
+        motor3.setInverted(true);motor2= new WPI_TalonSRX(26);
+        motor3.configFactoryDefault();
+        motor3.setInverted(true);
+        motor4= new WPI_TalonSRX(26);
+        motor4.configFactoryDefault();
+        motor4.setInverted(true);
+
+
 
 
 
@@ -52,17 +62,23 @@ public class Robot2 extends TimedRobot {
     public void teleopPeriodic() {
         if(controller.getLeftY()<-0.2||controller.getLeftY()>0.2) {
             motor.set(controller.getLeftY());
-            motor2.set(controller.getLeftY())
+            motor2.set(controller.getLeftY());
+            motor3.set(controller.getLeftY());
+            motor4.set(controller.getLeftY());
         }
         else{
             motor.set(0);
         }
 
         if(controller.getRightX()>0.2) {
+            motor4.set(controller.getRightX());
+            motor3.set(-(controller.getRightX())/2);
             motor2.set(controller.getRightX());
             motor.set(-(controller.getRightX())/2);
         }
         else if(controller.getRightX()<-0.2) {
+            motor4.set(-(controller.getRightX())/2);
+            motor3.set(controller.getRightX());
             motor2.set(-(controller.getRightX())/2);
             motor.set(controller.getRightX());
         }
